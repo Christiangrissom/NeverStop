bbac1e1433b4f45171acf830560dcedb7c6b4783
Christian <christiangrissom009@gmail.com>
2020-10-21 16:30:16 -0500

    Proces of creating superStack

---
+++ src/SuperStack.java
@@ -0,0 +1,27 @@
+
+public class SuperStack {
+	private T[] items;
+	private int top;
+	public SUperStack<T>(int size){
+		items = new T[];
+	}
+public boolean isEmpty() {
+	
+	return true;
+}
+public boolean isFull() {
+	
+	
+	return true;
+}
+public boolean push (T item) {
+	
+	return true;
+
+}
+public T pop() {
+	
+	
+	return null;
+}
+}
